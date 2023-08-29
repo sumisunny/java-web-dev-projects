4a5c7933ab8b6ab7f41b462f6c43efbfcede6a1e
ats <atsumitha@gmailcom>
2023-08-29 00:35:07 -0500

    Merge pull request #7 from LaunchCodeEducation/exceptions-starter
    Setting up starter code for exceptions chapter

--- datatypes/datatype-examples/src/main/java/org/launchcode/studios/areaofaCircle/Area.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/studios/areaofaCircle/Area.java
@@ -1,16 +1,16 @@
-package org.launchcode.studios.areaofacircle;
+package org.launchcode.studios.areaofaCircle;
 
 import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please enter radius of a circle");
+        System.out.print("Enter a radius: ");
         double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius " + radius + " is: " + area);
-
         input.close();
+
+        double area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
     }
 }
-
---
+++ datatypes/datatype-examples/src/main/java/org/launchcode/studios/areaofaCircle/Circle.java
@@ -0,0 +1,8 @@
+package org.launchcode.studios.areaofaCircle;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+    }
+}
+
